516a4df6a7b808febfe7f8aef68c0cdecca2860a
Pierre Chalamet <pierre@chalamet.net>
2018-04-04 21:37:37 +0200

    better error management

--- tools/scripts/demo.psm1
+++ tools/scripts/demo.psm1
@@ -1,20 +1,24 @@
 
 function Build-Devpac
 {
+    [CmdletBinding()]
     param([String] $out, [String[]] $files)
 
     vc -devpac +aos68k -O2 -notmpfile -nostdlib -o $out $files
+    if (! $?) { Throw "Compilation failure" }
 }
 
 function Invoke-AmigaExecutable
 {
+    [CmdletBinding()]
     param([String] $exe, [String] $config)
 
     $filename = Split-Path $exe -leaf
-    Copy-Item $exe ../../tools/hd0/$filename
-    New-Item ../../tools/hd0/S -ItemType Directory -Force
-    $filename | Out-File ../../tools/hd0/S/startup-sequence
+    Copy-Item $exe ../../tools/hd0/$filename -ErrorAction Stop
+    New-Item ../../tools/hd0/S -ItemType Directory -Force -ErrorAction Stop
+    $filename | Out-File ../../tools/hd0/S/startup-sequence -ErrorAction Stop
     fs-uae ../../tools/fs-uae/$config.fs-uae
+    if (! $?) { Throw "Execution failure" }
 }
 
 Export-ModuleMember -Function Build-Devpac
