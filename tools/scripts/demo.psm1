62d691292203618f407fe505cfe60186d4e00cc0
Pierre Chalamet <pierre@chalamet.net>
2018-04-05 21:49:39 +0200

    quiet mode

--- tools/scripts/demo.psm1
+++ tools/scripts/demo.psm1
@@ -4,7 +4,7 @@ function Build-Devpac
     [CmdletBinding()]
     param([String] $out, [String[]] $files)
 
-    vc -devpac +aos68k -O2 -notmpfile -nostdlib -o $out $files
+    vc -quiet -devpac +aos68k -O2 -notmpfile -nostdlib -o $out $files
     if (! $?) { Throw "Compilation failure" }
 }
 
@@ -15,7 +15,7 @@ function Invoke-AmigaExecutable
 
     $filename = Split-Path $exe -leaf
     Copy-Item $exe ../../tools/hd0/$filename -ErrorAction Stop
-    New-Item ../../tools/hd0/S -ItemType Directory -Force -ErrorAction Stop
+    New-Item ../../tools/hd0/S -ItemType Directory -Force -ErrorAction Stop | Out-Null
     $filename | Out-File ../../tools/hd0/S/startup-sequence -ErrorAction Stop
     fs-uae ../../tools/fs-uae/$config.fs-uae
     if (! $?) { Throw "Execution failure" }
